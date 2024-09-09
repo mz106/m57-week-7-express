27395f951a5813c79424a93978ddc75d99dd5033
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-09 15:24:32 +0100

    add basic routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const fakeDB = [];
+
+app.get("/books", (request, response) => {
+  const book = {
+    title: "book1",
+    author: "dave",
+    genre: "horror",
+  };
+  response.send({ message: "success", book: book });
+});
+
+// get all the books (i.e. fakeDB)
+
+// post
+
+app.post("/books", (request, response) => {
+  fakeDB.push(request.body);
+
+  response.send({ message: `${request.body.title} has been added` });
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
