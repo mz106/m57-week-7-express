38b1dd49b2677c2dc528e90d9fe656b4017f3b67
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-09 13:49:13 +0100

    remove static serve code

--- src/server.js
+++ src/server.js
@@ -2,11 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// http://localhost:5000/beetlejuice
-// https://mycoolwebsite/beetlejuice
-
-app.use("/beetlejuice", express.static("beetlejuice"));
-
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
