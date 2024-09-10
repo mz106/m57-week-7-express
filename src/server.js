1f034036a407138cb25d1b3e5571b8f412578843
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-10 10:52:25 +0100

    add routes

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const fakeDB = [];
 
-app.get("/books", (request, response) => {
+app.get("/books/onebook", (request, response) => {
   const book = {
     title: "book1",
     author: "dave",
@@ -17,12 +17,24 @@ app.get("/books", (request, response) => {
 
 // get all the books (i.e. fakeDB)
 
+app.get("/books/allbooks", (request, response) => {
+  response.send("hello all books");
+});
+
 // post
 
-app.post("/books", (request, response) => {
+app.post("/books/addbook", (request, response) => {
   fakeDB.push(request.body);
 
   response.send({ message: `${request.body.title} has been added` });
+});
+
+app.get("/movies/onemovie", (request, response) => {
+  response.send("One movie");
+});
+
+app.put("/movies/updatemovie", (request, response) => {
+  response.send("update movie");
 });
 
 app.listen(5000, () => {
