aba7c845e63a9c9b803d55721c005004d77bf6cc
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-10 15:26:17 +0100

    add post route for books

--- src/server.js
+++ src/server.js
@@ -1,22 +1,59 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
+// DB connection
+
+const connection = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("DB is working");
+};
+
+connection();
+
+// Book model
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("book", bookSchema);
+
+// Routes
+
+// post
+app.post("/books/addbook", async (request, response) => {
+  const book = await Book.create({
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+
+  response.send({ message: "success", book: book });
+});
+
 app.get("/books/onebook", (request, response) => {
   response.send({ message: "success" });
 });
 
-// get all the books (i.e. fakeDB)
+// get all the books
 
 app.get("/books/allbooks", (request, response) => {
-  response.send({ message: "success" });
-});
-
-// post
-
-app.post("/books/addbook", (request, response) => {
   response.send({ message: "success" });
 });
 
